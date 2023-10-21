aba19a9ec8d39779989ca2c21e74f271125c9c7a
Dob <lalilulelo.mg@gmail.com>
2023-10-22 06:20:06 +1100

    added a comment

--- src/app/(Web-Pages)/motorcycle-shop/page.tsx
+++ src/app/(Web-Pages)/motorcycle-shop/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/nextjs'
 export default async function page() {
 	await checkUserAccess('motorcycle-shop')
 
-
+	//TODO :  add navigation and proceeed with app from here you can check access and send user home if they dont trough te checkuseraccess function
 	return (
 		<div>
 			<UserButton afterSignOutUrl="/" />
